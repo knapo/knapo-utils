c3a97a0f149903f28fb28cd22ef5df3ed77abe0b
knapo <knapo@knapo.net>
2012-06-09 00:42:49 +0200

    Add picombiner.rb

--- picombiner.rb
+++ picombiner.rb
@@ -8,33 +8,40 @@
 #
 # Usage:
 # 
-# `$ ruby picombiner.rb [dir] [border_size]`
+# `$ ruby picombiner.rb [dir]`
 #
 
 require 'fileutils'
 
 FILE_RULE = "*.{JPG,jpg,PNG,png,TIF,tif}"
+COLORS = ['white', 'black']
+BORDER_FACTOR = 0.0125
+DIRECTIONS = {:horizontal => "+append", :vertical => "-append"}
+
 @@in_dir  = File.directory?(ARGV[0]) ? ARGV[0] : File.expand_path(File.join(File.dirname(__FILE__), ARGV[0]))
 @@tmp_dir = File.join(@@in_dir, 'tmp')
-
-BORDER_COLOR = 'black'
-@@border_width = ARGV[1].to_i
-@@border_width = 50 if @@border_width == 0
+@@out_dir = File.join(@@in_dir, 'out')
 
 @@files = Dir[File.join(@@in_dir, FILE_RULE)]
+raise "No files found!" if @@files.empty?
+
+@@base_border = (`identify -format "%w" #{@@files.first}`.to_f * BORDER_FACTOR).to_i
+
+FileUtils.rm_rf(@@out_dir)
+FileUtils.mkdir_p(@@out_dir)
 
 def files_in(dir)
   Dir[File.join(dir, FILE_RULE)]
 end
 
-def output_file
+def output_file(direction, color)
   first_file = Dir[File.join(@@in_dir, FILE_RULE)].sort.first
-  file_name = [File.basename(first_file, ".*"), 'k', File.extname(first_file)].join
-  return File.join(@@in_dir, file_name)
+  file_name = [File.basename(first_file, ".*"), direction[0], color[0], File.extname(first_file)].join
+  return File.join(@@out_dir, file_name)
 end
 
 def picture_border(idx, total)
-  return (idx == 0 || idx == total-1) ? @@border_width : 0
+  return (idx == 0 || idx == total-1) ? @@base_border : 0
 end
 
 def exec(cmd)
@@ -42,24 +49,36 @@ def exec(cmd)
   `#{cmd}`
 end
 
-def border_each!
-  files = @@files.select{|f| f != output_file}
-  files.each_with_index do |f, idx|
-    file_path = File.join(@@tmp_dir, File.basename(f))
-    border = picture_border(idx, files.size)
-    exec "convert -bordercolor #{BORDER_COLOR} -border #{border}x#{@@border_width} '#{f}' '#{file_path}'"
+def border_im_operator(border, direction)
+  case direction 
+    when :horizontal then "#{border}x#{@@base_border}"
+    when :vertical then "#{@@base_border}x#{border}"
+    else raise "Invalid IM direction"
   end
 end
 
-def combine!
-  file_list = Dir[File.join(@@tmp_dir, FILE_RULE)].join(' ')
-  exec "convert #{file_list} +append '#{output_file}'"
+def border_each!(direction, color)
+  @@files.each_with_index do |f, idx|
+    file_path = File.join(@@tmp_dir, File.basename(f))
+    current_border = picture_border(idx, @@files.size)
+    exec "convert -bordercolor #{color} -border #{border_im_operator(current_border, direction)} '#{f}' '#{file_path}'"
+  end
 end
 
-begin
-  FileUtils.mkdir_p(@@tmp_dir)
-  border_each!
-  combine!
-ensure
-  FileUtils.rm_rf(@@tmp_dir)
+def combine!(direction, color)
+  file_list = Dir[File.join(@@tmp_dir, FILE_RULE)].join(' ')
+  exec "convert #{file_list} #{DIRECTIONS[direction]} '#{output_file(direction, color)}'"
+end
+
+DIRECTIONS.keys.each do |direction|
+  COLORS.each do |color|
+    puts "*** #{direction} bordering with #{color} #{@@base_border}px ***" 
+    begin
+      FileUtils.mkdir_p(@@tmp_dir)
+      border_each!(direction, color)
+      combine!(direction, color)
+    ensure
+      FileUtils.rm_rf(@@tmp_dir)
+   end
+  end
 end
